aa5e3a5f7697904af2cef77def5ebba6dd250e79
Ferdinando Traversa <ferdi.traversa@gmail.com>
2020-06-10 20:37:44 +0200

    fix alla query

--- nofoto.php
+++ nofoto.php
@@ -10,39 +10,74 @@
 
 	$commons = \wm\Commons::instance();
 	
-	
 	if ($mode == 1) {
 		$file = fopen("elementi.txt", "w+");
 			// Wikidata SPARQL Query
 	$query = <<<QUERY
-SELECT DISTINCT ?item ?itemLabel ?coords ?wlmid ?image
+SELECT ?item ?itemLabel ?coords ?wlmid ?image
 WHERE {
 ?item wdt:P2186 ?wlmid ;
 					wdt:P17 wd:Q38 ;
 MINUS { ?item wdt:P18 ?image }
 MINUS {?item wdt:P373 ?cat}
-		MINUS { ?item p:P2186 [ pq:P582 ?end ] .
-		FILTER ( ?end <= "2020-09-01T00:00:00+00:00"^^xsd:dateTime )
-					}
-SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],it" }
+MINUS {
+    ?wlmst pqv:P580 [ wikibase:timeValue ?start ; wikibase:timePrecision ?sprec ] .
+    FILTER (
+      # precisione 9 è l'anno
+      ( ?sprec >  9 && ?start >= "2020-10-01T00:00:00+00:00"^^xsd:dateTime ) ||
+      ( ?sprec < 10 && ?start >= "2021-01-01T00:00:00+00:00"^^xsd:dateTime )
+    )
+  }
+  # esclude i monumenti che hanno una data di termine precedente all'inizio del concorso
+  MINUS {
+    ?wlmst pqv:P582 [ wikibase:timeValue ?end ; wikibase:timePrecision ?eprec ] .
+    FILTER (
+      ( ?eprec >  9 && ?end < "2020-09-01T00:00:00+00:00"^^xsd:dateTime ) ||
+      ( ?eprec < 10 && ?end < "2020-01-01T00:00:00+00:00"^^xsd:dateTime )
+    )
+  }
+  # esclude i monumenti per cui è indicata una data con un anno diverso da quello del concorso
+  MINUS {
+    ?wlmst pq:P585 ?date .
+    FILTER ( ?date < "2020-01-01T00:00:00+00:00"^^xsd:dateTime || ?date >= "2021-01-01T00:00:00+00:00"^^xsd:dateTime )
+  }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],it" }
 	}
 QUERY;
 
 if ($regione == "Puglia") {
 	$query = <<<QUERY
-	SELECT DISTINCT ?item ?itemLabel ?wlmid
-	WHERE {
-	?item wdt:P2186 ?wlmid .
-	?item wdt:P131* wd:Q1447 .
-						?item wdt:P17 wd:Q38 .
-	MINUS { ?item wdt:P18 ?image }
-	MINUS { ?item wdt:P373 ?cat }
-			MINUS { ?item p:P2186 [ pq:P582 ?end ] .
-			FILTER ( ?end <= "2020-09-01T00:00:00+00:00"^^xsd:dateTime )}
-	SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],it" }
+		SELECT ?item ?itemLabel ?wlmid
+		WHERE {
+		?item wdt:P2186 ?wlmid .
+		?item wdt:P131* wd:Q1447 .
+							?item wdt:P17 wd:Q38 .
+		MINUS { ?item wdt:P18 ?image }
+		MINUS { ?item wdt:P373 ?cat }
+		MINUS {
+			?wlmst pqv:P580 [ wikibase:timeValue ?start ; wikibase:timePrecision ?sprec ] .
+			FILTER (
+			# precisione 9 è l'anno
+			( ?sprec >  9 && ?start >= "2020-10-01T00:00:00+00:00"^^xsd:dateTime ) ||
+			( ?sprec < 10 && ?start >= "2021-01-01T00:00:00+00:00"^^xsd:dateTime )
+			)
 		}
-
+		# esclude i monumenti che hanno una data di termine precedente all'inizio del concorso
+		MINUS {
+			?wlmst pqv:P582 [ wikibase:timeValue ?end ; wikibase:timePrecision ?eprec ] .
+			FILTER (
+			( ?eprec >  9 && ?end < "2020-09-01T00:00:00+00:00"^^xsd:dateTime ) ||
+			( ?eprec < 10 && ?end < "2020-01-01T00:00:00+00:00"^^xsd:dateTime )
+			)
+		}
+		# esclude i monumenti per cui è indicata una data con un anno diverso da quello del concorso
+		MINUS {
+			?wlmst pq:P585 ?date .
+			FILTER ( ?date < "2020-01-01T00:00:00+00:00"^^xsd:dateTime || ?date >= "2021-01-01T00:00:00+00:00"^^xsd:dateTime )
+		}	SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],it" }
+			}
 QUERY;
+	$file = fopen("elementi" . $regione . ".txt", "w+");
 	}
 		$rows = \wm\Wikidata::querySPARQL( $query );
 	
@@ -50,7 +85,7 @@ QUERY;
 		$elnum = 0;
 		foreach( $rows as $row ) {
 			// Mettere che si evita di procedere con file diversi
-			$search = '"' . $row->wlmid->value. '" hastemplate:Monumento italiano';
+			$search = '"' . $row->wlmid->value. '"';
 			$response = $commons->fetch([
 				'action' => 'query',
 				'list' => 'search',
@@ -77,14 +112,17 @@ QUERY;
 	}
 	if ($mode == 2) {
 		$file = fopen("elementi.txt", "r");
+		if ($regione == "Puglia") {
+			$file = fopen("elementi" . $regione . ".txt", "w+");
+		}
+		$new = fopen("nuovielementi.txt", "w+");
 		$fileread = fread($file, filesize("elementi.txt"));
 		$elements = explode("\n", $fileread);
 		$elcount = count($elements);
 		$total = 0;
 		foreach ($elements as $element)	 {
 			$el = explode(";", $element);
-			$search = '"' . $el[0]. '" hastemplate:Monumento italiano';
-			echo('Controllo ' . $el[1] . "\n");
+			$search = '"' . $el[0]. '"';
 			$response = $commons->fetch([
 				'action' => 'query',
 				'list' => 'search',
@@ -99,12 +137,16 @@ QUERY;
 			}
 			if($count > 0) {
 				$total++;
+				$string = $element . "\n";
+				fwrite($new, $string);
 			}
+			echo('Controllo ' . $el[1] . " - " . $total .  " monuementi con una foto in più.\n");
 		}	
 		echo("Monumenti che prima non avevano un'immagine: " . $elcount . "\n");
 		echo("Monumenti che ora non hanno un'immagine: " . $elcount - $total . "\n");
 		echo("Nuovi monumenti con immagine: " . $total . "\n");
-
+		fclose($file);
+		fclose($new);
 	}
 
 ?>
